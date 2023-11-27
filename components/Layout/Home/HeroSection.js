504c9368664447c7fa7498f07dc27de6dea8cb6b
opulencedeveloper <victorkudos@gmail.com>
2023-11-27 13:25:28 +0100

    victor initial commit

--- components/Layout/Home/HeroSection.js
+++ components/Layout/Home/HeroSection.js
@@ -1,5 +1,20 @@
 const HeroSection = () => {
-  return <section></section>;
+  return (
+    <section>
+      <div>
+        {" "}
+        <div>
+          <p>
+            Don’t Settle for Less. Achieve Business Excellence with Marshall
+            Goodman Industries.
+          </p>
+          <p>
+             Kickstart Your Business Visibility With The Ultimate IT Partner
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
