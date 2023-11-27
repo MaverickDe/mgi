e000c36343afbdc73cd1b529f53ec1a60d114ddb
opulencedeveloper <victorkudos@gmail.com>
2023-11-27 14:57:37 +0100

    victor initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-
 import HeroSection from "@/components/Home/HeroSection";
+import Overview from "@/components/Home/Overview";
 import Layout from "@/components/Layout/Layout";
 
 export default function Home() {
   return ( 
       <Layout>
         <HeroSection />
+        <Overview ></Overview>
       </Layout>
  
   );
